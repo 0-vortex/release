dbdc0e26d93a2ebc9051d1cb8c2dd50ccb48feb8
TED Vortex <ted.vortex@gmail.com>
2021-07-29 01:07:05 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# @semantic-release/apm-config
+
+[**semantic-release**](https://github.com/semantic-release/semantic-release) shareable config to publish to `npm` and/or `ghcr`.
+
+## Plugins
+
+This shareable configuration use the following plugins:
+
+- [`@semantic-release/commit-analyzer`](https://github.com/semantic-release/commit-analyzer)
+- [`@semantic-release/release-notes-generator`](https://github.com/semantic-release/release-notes-generator)
+- [`@semantic-release/changelog`](https://github.com/semantic-release/changelog)
+- [`@semantic-release/npm`](https://github.com/semantic-release/npm)
+- [`@semantic-release/git`](https://github.com/semantic-release/git)
+- [`@semantic-release/github`](https://github.com/semantic-release/github)
+- [`@eclass/semantic-release-docker`](https://github.com/eclass/semantic-release-docker)
+
+## Install
+
+```bash
+$ npm install --save-dev semantic-release demo-test-artifact-2
+```
+
+## Usage
+
+The shareable config can be configured in the [**semantic-release** configuration file](https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration):
+
+```json
+{
+  "extends": "demo-test-artifact-2"
+}
+```
+
+## Configuration
+
+See each [plugin](#plugins) documentation for required installation and configuration steps.
---
+++ release.config.js
@@ -0,0 +1,82 @@
+const plugins = [
+  ["@semantic-release/commit-analyzer", {
+    "preset": "conventionalcommits",
+    "releaseRules": [
+      {type: "build", release: "minor"},
+      {type: "ci", release: "patch"},
+      {type: "docs", release: "minor"},
+      {type: "style", release: "patch"},
+      {type: "refactor", release: "patch"},
+      {type: "test", release: "patch"},
+      {type: "revert", release: "patch"},
+      {type: "chore", release: false}
+    ],
+    "parserOpts": {
+      "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+    }
+  }],
+  ["@semantic-release/release-notes-generator", {
+    "preset": "conventionalcommits",
+    "parserOpts": {
+      "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+    },
+    "writerOpts": {
+      "commitsSort": ["subject", "scope"]
+    },
+    "presetConfig": {
+      types: [
+        {type: 'feat', section: 'Features'},
+        {type: 'feature', section: 'Features'},
+        {type: 'fix', section: 'Bug Fixes'},
+        {type: 'perf', section: 'Performance Improvements'},
+        {type: 'revert', section: 'Reverts'},
+        {type: 'docs', section: 'Documentation'},
+        {type: 'style', section: 'Styles'},
+        {type: 'refactor', section: 'Code Refactoring'},
+        {type: 'test', section: 'Tests'},
+        {type: 'build', section: 'Build System'},
+        {type: 'ci', section: 'Continuous Integration'}
+      ]
+    }
+  }],
+  "@semantic-release/changelog",
+  ["@semantic-release/npm", {
+    "tarballDir": "pack"
+  }],
+  ["@semantic-release/git", {
+    "assets": [
+      "CHANGELOG.md",
+      "package.json",
+      "npm-shrinkwrap.json"
+    ],
+    "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
+  }],
+  ["@semantic-release/github", {
+    "assets": [
+      {
+        "path": "pack/*.tgz",
+        "label": "Static distribution"
+      }
+    ]
+  }],
+];
+
+!process.env.DISABLE_DOCKER && plugins.push([
+  "@eclass/semantic-release-docker",
+  {
+    "baseImageName": `${process.env.GITHUB_REPOSITORY}`,
+    "registries": [
+      {
+        "url": "ghcr.io",
+        "imageName": `ghcr.io/${process.env.GITHUB_REPOSITORY}`,
+        "user": "REPO_OWNER",
+        "password": "GITHUB_TOKEN"
+      }
+    ]
+  }
+]);
+
+module.exports = {
+  "branches": ["main"],
+  plugins,
+}
