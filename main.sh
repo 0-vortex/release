5588ce76a23fd3a72e8dd0c80f2334bac77cb0ea
TED Vortex <ted.vortex@gmail.com>
2021-11-18 01:28:19 +0200

    fix: correct incomplete if statement

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ semantic-release -d | tee semantic-release-output.txt
 
 version=$(cat semantic-release-output.txt | grep "The next release version is" | rev | cut -d' ' -f1 | rev)
 
-f [ ! -z "$version" ]; then
+if [ ! -z "$version" ]; then
   echo "::set-output name=released-version::v$version"
   echo "::set-output name=released-version-number::$version"
 fi
