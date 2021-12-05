d7f36717c6b480f331b9917d61b17a6ea684dafa
TED Vortex <ted.vortex@gmail.com>
2021-12-05 11:48:10 +0200

    fix: learn to write javascript again

--- release.config.js
+++ release.config.js
@@ -141,7 +141,7 @@ addPlugin("@semantic-release/git", {
     "public/diagram.svg",
     "action.yml"
   ],
-  "message": `chore(release): \${nextRelease.version}\${nextRelease.channel && \` on \${nextRelease.channel} channel\`} [skip ci]\n\n\${nextRelease.notes}`
+  "message": `chore(release): \${nextRelease.version}\${nextRelease.channel ? \` on \${nextRelease.channel} channel\` : ''} [skip ci]\n\n\${nextRelease.notes}`
 });
 
 addPlugin("@semantic-release/github", {
