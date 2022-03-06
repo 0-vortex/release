e4e67e32105ac3f168a45eea912c99d89e186bf0
TED Vortex <ted.vortex@gmail.com>
2022-03-06 22:23:19 +0200

    feat: revert to v1.4.0
    BREAKING CHANGE: test 1.4.0 bc

--- release.config.js
+++ release.config.js
@@ -1,207 +1,83 @@
-const { resolve } = require('path');
-const { existsSync } = require("fs");
-const { sync, commandSync } = require("execa");
-const log = require("npmlog");
-
-const plugins = [];
-const noteKeywords = [
-  "BREAKING CHANGE",
-  "BREAKING CHANGES",
-  "BREAKING"
-];
-const {
-  GITHUB_SHA,
-  GITHUB_REPOSITORY,
-  GIT_COMMITTER_NAME,
-  GIT_COMMITTER_EMAIL,
-  GIT_AUTHOR_NAME,
-  GIT_AUTHOR_EMAIL,
-} = process.env;
-const successCmd = `
-echo 'RELEASE_TAG=v\${nextRelease.version}' >> $GITHUB_ENV
-echo 'RELEASE_VERSION=\${nextRelease.version}' >> $GITHUB_ENV
-echo '::set-output name=release-tag::v\${nextRelease.version}'
-echo '::set-output name=release-version::\${nextRelease.version}'
-`;
-const [owner, repo] = String(GITHUB_REPOSITORY).toLowerCase().split("/");
-const addPlugin = (plugin, options) => {
-  log.info(`${plugin} enabled ${options && 'with options:'}`);
-  options && log.info(null, options);
-  return plugins.push([plugin, options]);
-};
-
-log.info(`Executing semantic-release config setup`);
-
-!GIT_COMMITTER_NAME && (process.env.GIT_COMMITTER_NAME = "open-sauced[bot]");
-!GIT_COMMITTER_EMAIL && (process.env.GIT_COMMITTER_EMAIL = "63161813+open-sauced[bot]@users.noreply.github.com");
-
-try {
-  const { stdout: authorName } = sync("git", ["log", "-1", "--pretty=format:%an", GITHUB_SHA]);
-  const { stdout: authorEmail } = sync("git", ["log", "-1", "--pretty=format:%ae", GITHUB_SHA]);
-  authorName && !GIT_AUTHOR_NAME && (process.env.GIT_AUTHOR_NAME = `${authorName}`);
-  authorEmail && !GIT_AUTHOR_EMAIL && (process.env.GIT_AUTHOR_EMAIL = `${authorEmail}`);
-} catch (e) {
-  log.error(`Unable to set GIT_COMMITTER_NAME and GIT_COMMITTER_EMAIL`, e);
-}
-
-addPlugin("@semantic-release/commit-analyzer", {
-  "preset": "conventionalcommits",
-  "releaseRules": [
-    {type: "feat", release: "minor"},
-    {type: "fix", release: "patch"},
-    {type: "perf", release: "patch"},
-    {type: "revert", release: "patch"},
-    {type: "docs", release: "minor"},
-    {type: "style", release: "patch"},
-    {type: "refactor", release: "patch"},
-    {type: "test", release: "patch"},
-    {type: "build", release: "patch"},
-    {type: "ci", release: "patch"},
-    {type: "chore", release: false}
-  ],
-  "parserOpts": {
-    noteKeywords
-  }
-});
-
-addPlugin("@semantic-release/release-notes-generator", {
-  "preset": "conventionalcommits",
-  "parserOpts": {
-    noteKeywords
-  },
-  "writerOpts": {
-    "commitsSort": ["subject", "scope"]
-  },
-  "presetConfig": {
-    types: [
-      {type: "feat", section: "🍕 Features"},
-      {type: "feature", section: "🍕 Features"},
-      {type: "fix", section: "🐛 Bug Fixes"},
-      {type: "perf", section: "🔥 Performance Improvements"},
-      {type: "revert", section: "⏩ Reverts"},
-      {type: "docs", section: "📝 Documentation"},
-      {type: "style", section: "🎨 Styles"},
-      {type: "refactor", section: "🧑‍💻 Code Refactoring"},
-      {type: "test", section: "✅ Tests"},
-      {type: "build", section: "🤖 Build System"},
-      {type: "ci", section: "🔁 Continuous Integration"}
+const plugins = [
+  ["@semantic-release/commit-analyzer", {
+    "preset": "conventionalcommits",
+    "releaseRules": [
+      {type: "build", release: "minor"},
+      {type: "ci", release: "patch"},
+      {type: "docs", release: "minor"},
+      {type: "style", release: "patch"},
+      {type: "refactor", release: "patch"},
+      {type: "test", release: "patch"},
+      {type: "revert", release: "patch"},
+      {type: "chore", release: false}
+    ],
+    "parserOpts": {
+      "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+    }
+  }],
+  ["@semantic-release/release-notes-generator", {
+    "preset": "conventionalcommits",
+    "parserOpts": {
+      "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+    },
+    "writerOpts": {
+      "commitsSort": ["subject", "scope"]
+    },
+    "presetConfig": {
+      types: [
+        {type: 'feat', section: 'Features'},
+        {type: 'feature', section: 'Features'},
+        {type: 'fix', section: 'Bug Fixes'},
+        {type: 'perf', section: 'Performance Improvements'},
+        {type: 'revert', section: 'Reverts'},
+        {type: 'docs', section: 'Documentation'},
+        {type: 'style', section: 'Styles'},
+        {type: 'refactor', section: 'Code Refactoring'},
+        {type: 'test', section: 'Tests'},
+        {type: 'build', section: 'Build System'},
+        {type: 'ci', section: 'Continuous Integration'}
+      ]
+    }
+  }],
+  "@semantic-release/changelog",
+  ["@semantic-release/npm", {
+    "tarballDir": "pack"
+  }],
+  ["@semantic-release/git", {
+    "assets": [
+      "CHANGELOG.md",
+      "package.json",
+      "npm-shrinkwrap.json",
+      "public/diagram.svg"
+    ],
+    "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
+  }],
+  ["@semantic-release/github", {
+    "assets": [
+      {
+        "path": "pack/*.tgz",
+        "label": "Static distribution"
+      }
     ]
-  }
-});
+  }],
+];
 
-addPlugin("@semantic-release/changelog", {
-  "changelogTitle": `# 📦 ${owner}/${repo} changelog
-
-[![conventional commits](https://img.shields.io/badge/conventional%20commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
-[![semantic versioning](https://img.shields.io/badge/semantic%20versioning-2.0.0-green.svg)](https://semver.org)
-
-> All notable changes to this project will be documented in this file`
-});
-
-addPlugin("@semantic-release/npm", {
-  "tarballDir": "pack"
-});
-
-const actionExists = existsSync("./action.yml");
-if (actionExists) {
-  addPlugin("@google/semantic-release-replace-plugin", {
-    "replacements": [{
-      "files": [
-        "action.yml"
-      ],
-      "from": `image: 'docker://ghcr.io/${owner}/${repo}:.*'`,
-      "to": `image: 'docker://ghcr.io/${owner}/${repo}:\${nextRelease.version}'`,
-      "results": [{
-        "file": "action.yml",
-        "hasChanged": true,
-        "numMatches": 1,
-        "numReplacements": 1
-      }],
-      "countMatches": true
-    }]
-  });
-}
-
-try {
-  const {stdout} = commandSync("ls -A1 LICENSE*", {
-    shell: true,
-  });
-
-  addPlugin("semantic-release-license", {
-    license: {
-      path: resolve(stdout)
-    }
-  });
-} catch (e) {
-  log.error(`Unable to run detect license command`, e);
-}
-
-addPlugin("@semantic-release/git", {
-  "assets": [
-    "LICENSE*",
-    "CHANGELOG.md",
-    "package.json",
-    "package-lock.json",
-    "npm-shrinkwrap.json",
-    "yarn.lock",
-    "pnpm-lock.yaml",
-    "public/**/*",
-    "supabase/**/*",
-    "action.yml"
-  ],
-  "message": `chore(<%= nextRelease.type %>): release <%= nextRelease.version %> <%= nextRelease.channel !== null ? \`on \${nextRelease.channel} channel \` : '' %>[skip ci]\n\n<%= nextRelease.notes %>`
-});
-
-addPlugin("@semantic-release/github", {
-  "addReleases": "bottom",
-  "assets": [
-    {
-      "path": "pack/*.tgz",
-      "label": "Static distribution"
-    }
-  ]
-});
-
-const dockerExists = existsSync("./Dockerfile");
-if (dockerExists) {
-  addPlugin("eclass-docker-fork", {
-    "baseImageName": `${owner}/${repo}`,
+!process.env.DISABLE_DOCKER && plugins.push([
+  "@eclass/semantic-release-docker",
+  {
+    "baseImageName": `${process.env.GITHUB_REPOSITORY}`,
     "registries": [
       {
         "url": "ghcr.io",
-        "imageName": `ghcr.io/${owner}/${repo}`,
-        "user": "GITHUB_REPOSITORY_OWNER",
+        "imageName": `ghcr.io/${process.env.GITHUB_REPOSITORY}`,
+        "user": "REPO_OWNER",
         "password": "GITHUB_TOKEN"
       }
     ]
-  });
-}
-
-if (process.env.GITHUB_ACTIONS !== undefined) {
-  addPlugin("@semantic-release/exec", {
-    successCmd,
-  });
-}
+  }
+]);
 
 module.exports = {
-  "branches": [
-    // maintenance releases
-    "+([0-9])?(.{+([0-9]),x}).x",
-
-    // release channels
-    "main",
-    "next",
-    "next-major",
-
-    // pre-releases
-    {
-      name: "beta",
-      prerelease: true
-    },
-    {
-      name: "alpha",
-      prerelease: true
-    }
-  ],
+  "branches": ["main"],
   plugins,
 }
