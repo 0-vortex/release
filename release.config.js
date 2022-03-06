7e376db015245f584b6c207c7b0d65f7ca96c041
TED Vortex <ted.vortex@gmail.com>
2022-03-07 01:04:06 +0200

    test: attempt to restore v3 functionality and bc
    BREAKING CHANGE: test v3 bc

--- release.config.js
+++ release.config.js
@@ -1,86 +1,113 @@
-const plugins = [
-  ["@semantic-release/commit-analyzer", {
-    "preset": "conventionalcommits",
-    "releaseRules": [
-      {type: "build", release: "minor"},
-      {type: "ci", release: "patch"},
-      {type: "docs", release: "minor"},
-      {type: "style", release: "patch"},
-      {type: "refactor", release: "patch"},
-      {type: "test", release: "patch"},
-      {type: "revert", release: "patch"},
-      {type: "chore", release: false}
-    ],
-    "parserOpts": {
-      "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-    }
-  }],
-  ["@semantic-release/release-notes-generator", {
-    "preset": "conventionalcommits",
-    "parserOpts": {
-      "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-    },
-    "writerOpts": {
-      "commitsSort": ["subject", "scope"]
-    },
-    "presetConfig": {
-      types: [
-        {type: 'feat', section: 'Features'},
-        {type: 'feature', section: 'Features'},
-        {type: 'fix', section: 'Bug Fixes'},
-        {type: 'perf', section: 'Performance Improvements'},
-        {type: 'revert', section: 'Reverts'},
-        {type: 'docs', section: 'Documentation'},
-        {type: 'style', section: 'Styles'},
-        {type: 'refactor', section: 'Code Refactoring'},
-        {type: 'test', section: 'Tests'},
-        {type: 'build', section: 'Build System'},
-        {type: 'ci', section: 'Continuous Integration'}
-      ]
-    }
-  }],
-  "@semantic-release/changelog",
-  ["@semantic-release/npm", {
-    "tarballDir": "pack"
-  }],
-  ["@semantic-release/git", {
-    "assets": [
-      "CHANGELOG.md",
-      "package.json",
-      "npm-shrinkwrap.json",
-      "public/diagram.svg"
-    ],
-    "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
-  }],
-  ["@semantic-release/github", {
-    "assets": [
-      {
-        "path": "pack/*.tgz",
-        "label": "Static distribution"
-      }
-    ]
-  }],
-];
+const { existsSync } = require("fs");
+const { sync, commandSync } = require("execa");
+const log = require("npmlog");
 
-// !process.env.DISABLE_DOCKER && plugins.push([
-//   "@eclass/semantic-release-docker",
-//   {
-//     "baseImageName": `${process.env.GITHUB_REPOSITORY}`,
-//     "registries": [
-//       {
-//         "url": "ghcr.io",
-//         "imageName": `ghcr.io/${process.env.GITHUB_REPOSITORY}`,
-//         "user": "REPO_OWNER",
-//         "password": "GITHUB_TOKEN"
-//       }
-//     ]
-//   }
-// ]);
+const plugins = [];
+const noteKeywords = [
+  "BREAKING CHANGE",
+  "BREAKING CHANGES",
+  "BREAKING"
+];
+const {
+  GITHUB_SHA,
+  GITHUB_REPOSITORY,
+  GIT_COMMITTER_NAME,
+  GIT_COMMITTER_EMAIL,
+  GIT_AUTHOR_NAME,
+  GIT_AUTHOR_EMAIL,
+} = process.env;
+const successCmd = `
+echo 'RELEASE_TAG=v\${nextRelease.version}' >> $GITHUB_ENV
+echo 'RELEASE_VERSION=\${nextRelease.version}' >> $GITHUB_ENV
+echo '::set-output name=release-tag::v\${nextRelease.version}'
+echo '::set-output name=release-version::\${nextRelease.version}'
+`;
+const [owner, repo] = String(GITHUB_REPOSITORY).toLowerCase().split("/");
+const addPlugin = (plugin, options) => {
+  log.info(`${plugin} enabled ${options && 'with options:'}`);
+  options && log.info(null, options);
+  return plugins.push([plugin, options]);
+};
+
+log.info(`Executing semantic-release config setup`);
+
+!GIT_COMMITTER_NAME && (process.env.GIT_COMMITTER_NAME = "open-sauced[bot]");
+!GIT_COMMITTER_EMAIL && (process.env.GIT_COMMITTER_EMAIL = "63161813+open-sauced[bot]@users.noreply.github.com");
+
+addPlugin("@semantic-release/commit-analyzer", {
+  "preset": "conventionalcommits",
+  "releaseRules": [
+    {type: "feat", release: "minor"},
+    {type: "fix", release: "patch"},
+    {type: "perf", release: "patch"},
+    {type: "revert", release: "patch"},
+    {type: "docs", release: "minor"},
+    {type: "style", release: "patch"},
+    {type: "refactor", release: "patch"},
+    {type: "test", release: "patch"},
+    {type: "build", release: "patch"},
+    {type: "ci", release: "patch"},
+    {type: "chore", release: false}
+  ],
+  "parserOpts": {
+    noteKeywords
+  }
+});
+
+addPlugin("@semantic-release/release-notes-generator", {
+  "preset": "conventionalcommits",
+  "parserOpts": {
+    "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+  },
+  "writerOpts": {
+    "commitsSort": ["subject", "scope"]
+  },
+  "presetConfig": {
+    types: [
+      {type: 'feat', section: 'Features'},
+      {type: 'feature', section: 'Features'},
+      {type: 'fix', section: 'Bug Fixes'},
+      {type: 'perf', section: 'Performance Improvements'},
+      {type: 'revert', section: 'Reverts'},
+      {type: 'docs', section: 'Documentation'},
+      {type: 'style', section: 'Styles'},
+      {type: 'refactor', section: 'Code Refactoring'},
+      {type: 'test', section: 'Tests'},
+      {type: 'build', section: 'Build System'},
+      {type: 'ci', section: 'Continuous Integration'}
+    ]
+  }
+});
+
+addPlugin("@semantic-release/changelog");
+
+addPlugin("@semantic-release/npm", {
+  "tarballDir": "pack"
+});
+
+addPlugin("@semantic-release/git", {
+  "assets": [
+    "CHANGELOG.md",
+    "package.json",
+    "npm-shrinkwrap.json",
+    "public/diagram.svg"
+  ],
+  "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
+});
+
+addPlugin("@semantic-release/github", {
+  "assets": [
+    {
+      "path": "pack/*.tgz",
+      "label": "Static distribution"
+    }
+  ]
+});
 
 module.exports = {
   "branches": [
     // maintenance releases
-    // "+([0-9])?(.{+([0-9]),x}).x",
+    "+([0-9])?(.{+([0-9]),x}).x",
 
     // release channels
     "main",
