6fdc45777d5a892a1581359180d7a30b5a9a50c7
TED Vortex <ted.vortex@gmail.com>
2023-05-15 19:07:44 +0200

    test: test container re-issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ ls -lahH
 whoami
 pwd
 
+echo "$GITHUB_WORKSPACE"
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
 #time=$(date)
 #echo "time=$time" >> $GITHUB_OUTPUT
 npx semantic-release --extends /usr/local/lib/release.config.js
